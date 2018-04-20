c1a320d187eaeeb2a301ab71594eb675f642849c
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2018-04-20 18:30:42 +0200

    On card click add class 'open' and 'show'

--- js/app.js
+++ js/app.js
@@ -60,3 +60,19 @@ cards.forEach(card => {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+const openCards = new Array();
+
+cardsContainer.addEventListener('click', event => {
+    event.target.classList.add('show');
+    event.target.classList.add('open');
+    evaluateMatch();
+});
+
+
+function evaluateMatch() {
+    cards.forEach(card => {
+        console.log(card.htmlElement);
+    });
+}
